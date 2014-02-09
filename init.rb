c6bccf8ac084be9b1bf4df80f9b957d70f17f4fb
Steve Schwartz <steve@alfajango.com>
2014-02-09 16:31:45 -0500

    Created initial cashboard-tasks redmine plugin.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :cashboard_tasks do
+  name 'Cashboard Tasks plugin'
+  author 'Steve Schwartz'
+  description 'A plugin for Redmine that allows easily copying Redmine tasks over to Cashboard App.'
+  version '0.0.1'
+  url 'http://os.alfajango.com/redmine-cashboard-tasks'
+  author_url 'http://jangosteve.com'
+end
