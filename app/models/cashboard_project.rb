b5dfcbdd076fc320b9e43a2c63af95bf1ec11a03
Steve Schwartz <steve@alfajango.com>
2014-02-11 00:25:15 -0500

    Added validations to prevent from creating corrupted CashboardTask or CashboardProject entries.

--- app/models/cashboard_project.rb
+++ app/models/cashboard_project.rb
@@ -2,4 +2,6 @@ class CashboardProject < ActiveRecord::Base
   unloadable
 
   belongs_to :project
+
+  validates :project_id, :cashboard_project_id, :cashboard_project_name, :presence => true
 end
--- app/models/cashboard_task.rb
+++ app/models/cashboard_task.rb
@@ -2,4 +2,6 @@ class CashboardTask < ActiveRecord::Base
   unloadable
 
   belongs_to :issue
+
+  validates :issue_id, :cashboard_project_id, :cashboard_project_list_id, :cashboard_line_item_id, :presence => true
 end
