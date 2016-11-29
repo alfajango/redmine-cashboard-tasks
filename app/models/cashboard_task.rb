40e4746035785aaa289f004f29d533f3ef08fefc
Alec Tower <alectower@gmail.com>
2016-11-29 10:35:10 -0500

    Allow mass assign of cashboard task attributes

--- app/models/cashboard_task.rb
+++ app/models/cashboard_task.rb
@@ -3,5 +3,7 @@ class CashboardTask < ActiveRecord::Base
 
   belongs_to :issue
 
+  attr_accessible :issue_id, :cashboard_project_id, :cashboard_project_list_id, :cashboard_line_item_id
+
   validates :issue_id, :cashboard_project_id, :cashboard_project_list_id, :cashboard_line_item_id, :presence => true
 end
